8f0de7b07e5b20280c3d1ed04e241e4c3e919eca
arorpalak <60004971+arorpalak@users.noreply.github.com>
2020-06-22 19:54:43 -0700

    First Playable Build
    Added the character and the background setting

---
+++ README.md
@@ -0,0 +1,2 @@
+# arorpalak
+Assets of kitchen, yard, cafeteria, starting cell &amp; main area included 
---
+++ The_ZomB_Terminator/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,32 @@
+﻿
+/**This class is to control the movements of the bullet**/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour {
+
+	[SerializeField]
+	int damage = 5;
+
+	void Start () {
+		StartCoroutine ("Finish");
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		IDamageable d = other.gameObject.GetComponent<IDamageable> ();
+		if(d != null){
+			d.Damage (damage);
+	    }
+		Destroy (gameObject);
+   }
+
+	private IEnumerator Finish(){
+		yield return new WaitForSeconds (5);
+		Destroy (gameObject);
+	}
+}
+   
+public interface IDamageable{
+	void Damage (int damage);
+}
